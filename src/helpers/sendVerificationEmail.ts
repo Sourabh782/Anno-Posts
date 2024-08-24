78778f182b9abbcd0dbcbe179d6bf1f2f07b7431
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-08-24 13:30:31 +0530

    added signup route

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,25 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verificationCode: string
+): Promise<ApiResponse>{
+
+    try {
+        await resend.emails.send({
+            from: "hello",
+            to: email,
+            subject: "Anno-Posts Verification Code",
+            react: VerificationEmail({username, otp: verificationCode})
+        })
+
+        return {success: false, message: "Verification email sent successfully"}
+    } catch (emailError) {
+        console.error("error sending verification email", emailError);
+        return {success: false, message: "Failed to send verification email"}
+    }
+
+}
